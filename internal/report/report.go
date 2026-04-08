2caf14e3eaa9f2a6fa740deb14f97d3217bbbd58
kelyn <kelyn@crandall.me>
2026-04-07 23:08:45 -0500

    make text report more concise

--- internal/report/report.go
+++ internal/report/report.go
@@ -3,7 +3,6 @@ package report
 import (
 	"encoding/json"
 	"fmt"
-	"slices"
 	"strings"
 
 	"github.com/Henelik/css-trimmer/internal/diff"
@@ -37,23 +36,12 @@ func (r *Reporter) TextReport() string {
 		r.scannedFiles, defined, len(r.result.Used))
 
 	if len(r.result.ToRemove) > 0 {
-		fmt.Fprintf(&out, "  Removing %d classes:\n", len(r.result.ToRemove))
-
-		for _, className := range r.result.ToRemove {
-			reason := r.getRemovalReason(className)
-			fmt.Fprintf(&out, "    .%-24s (%s)\n", className, reason)
-		}
-
+		fmt.Fprintf(&out, "  Removing %d classes", len(r.result.ToRemove))
 		out.WriteString("\n")
 	}
 
 	if len(r.result.Whitelisted) > 0 {
-		fmt.Fprintf(&out, "  Keeping %d (whitelisted):\n", len(r.result.Whitelisted))
-
-		for _, className := range r.result.Whitelisted {
-			fmt.Fprintf(&out, "    .%s\n", className)
-		}
-
+		fmt.Fprintf(&out, "  Keeping %d (whitelisted)", len(r.result.Whitelisted))
 		out.WriteString("\n")
 	}
 
@@ -85,13 +73,4 @@ func (r *Reporter) JSONReport() string {
 
 	jsonBytes, _ := json.MarshalIndent(data, "", "  ")
 	return string(jsonBytes)
-}
-
-// getRemovalReason returns the reason why a class is being removed.
-func (r *Reporter) getRemovalReason(className string) string {
-	if slices.Contains(r.result.Blacklisted, className) {
-		return "blacklisted"
-	}
-
-	return "not referenced"
 }
